e0969867d4171fc58efd01b175a6b1d863854ce3
Aaditya785 <aadityasandil5@gmail.com>
2024-01-04 17:18:41 +0530

    docerized the files

---
+++ src/main/java/com/example/dockConnection/DockConnectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.dockConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockConnectionApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(DockConnectionApplication.class, args);
+	}
+
+}
